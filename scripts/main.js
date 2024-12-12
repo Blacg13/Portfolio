7f90a024d8acb5f7a5f6e11e741ed7c864556789
Bénédicte Lagasse <benedicte.lagasse@proton.me>
2024-12-12 15:54:51 +0100

    position fixed fixed and border with opacity gradient

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,29 @@
-const ACCORDION_TOGGLE_H2 = document.querySelectorAll("section > h2");
-const ACCORDION_CONTENT = document.querySelectorAll("section > div");
+const ACCORDION_TOGGLE_H2 = document.querySelectorAll('section > h2');
+const ACCORDION_CONTENT = document.querySelectorAll('section > div');
 ACCORDION_CONTENT.hidden = true;
 
+const HTML = document.querySelector('html');
+function switchDark() {
+  HTML.style.setProperty('color-scheme', 'dark');
+}
+
+function switchLight() {
+  HTML.style.setProperty('color-scheme', 'light');
+}
+
+function switchAuto() {
+  HTML.style.setProperty('color-scheme', 'light dark');
+}
+switchDark();
+
 ACCORDION_TOGGLE_H2.forEach((h2) => {
-    h2.addEventListener("click", () => {
-        console.log("clicked");
-        // ACCORDION_CONTENT.style.display = "block";
-        
-    });
+  h2.addEventListener('click', () => {
+    console.log('clicked');
+    // ACCORDION_CONTENT.style.display = "block";
+  });
 });
 for (let i = 0; i < ACCORDION_TOGGLE_H2.length; i++) {
-    ACCORDION_TOGGLE_H2[i].addEventListener("click", () => {
-        this.classList.toggle("active");
-    })
-    
+  ACCORDION_TOGGLE_H2[i].addEventListener('click', () => {
+    this.classList.toggle('active');
+  });
 }
